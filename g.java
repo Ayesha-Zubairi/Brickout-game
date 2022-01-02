49df2a4505aa4e59a15514e2ab2cf536132c8ca7
Ayesha-Zubairi <87482685+Ayesha-Zubairi@users.noreply.github.com>
2022-01-02 22:22:19 +0500

    Create g.java

---
+++ g.java
@@ -0,0 +1,25 @@
+package BrickBreaker;
+
+import java.awt.Color;
+import java.awt.Font;
+
+class g {
+
+    static void setColor(Color white) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setFont(Font font) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void drawString(String level_2, int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void fillRect(int playerX, int i, int i0, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
